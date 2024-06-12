e1660842de89dee4b18d0c2a4db8710b6fea173c
liu-roger <rogerliu80@gmail.com>
2024-06-12 17:22:33 -0400

    added video utilities

---
+++ utils/video_utils.py
@@ -0,0 +1,18 @@
+import cv2
+
+def read_video(vid_path):
+    cap = cv2.VideoCapture(vid_path)
+    frames = []
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        frames.append(frame)
+    return frames
+
+def save_video(output_vid_frames, output_vid_path):
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    out = cv2.VideoWriter(output_vid_path, fourcc, 24, (output_vid_frames[0].shape[1], output_vid_frames[0].shape[0]))
+    for frame in output_vid_frames:
+        out.write(frame)
+    out.release
