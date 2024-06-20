264441308fd9eb99d3789beea576f4cd8ce534d9
liu-roger <rogerliu80@gmail.com>
2024-06-20 12:57:34 -0400

    team colors tracked

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from utils import read_video, save_video
 from trackers import Tracker
-
+import cv2
+from team_assigner import TeamAssigner
 
 def main():
     print('hello world')
     # read video
     video_frames = read_video('input_videos/08fd33_4.mp4')
+
 
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
@@ -13,9 +15,26 @@ def main():
     tracks = tracker.get_object_tracks(video_frames,
                                        read_from_stub=True,
                                        stub_path='stubs/track_stubs.pkl')
+    
+    # Assign Player Teams
+    team_assigner = TeamAssigner()
+    team_assigner.assign_team_color(video_frames[0], 
+                                    tracks['players'][0])
+    
+    for frame_num, player_track in enumerate(tracks['players']):
+        for player_id, track in player_track.items():
+            team = team_assigner.get_player_team(video_frames[frame_num],   
+                                                 track['bbox'],
+                                                 player_id)
+            tracks['players'][frame_num][player_id]['team'] = team 
+            tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
+
+
+    # drawing outputs
+    output_video_frames = tracker.draw_annotations(video_frames, tracks)
 
     # save video
-    save_video(video_frames, 'output_videos/output_video.avi')
+    save_video(output_video_frames, 'output_videos/output_video.avi')
 
 if __name__ == '__main__':
     main()
--- trackers/tracker.py
+++ trackers/tracker.py
@@ -2,6 +2,11 @@ from ultralytics import YOLO
 import supervision as sv
 import pickle
 import os
+import cv2
+import numpy as np
+import sys
+sys.path.append('../')
+from utils import get_center_of_bbox, get_bbox_width
 
 class Tracker:
     def __init__(self, model_path):
@@ -16,7 +21,6 @@ class Tracker:
             detections_batch = self.model.predict(frames[i:i+batch_size], conf=0.1)
             detections += detections_batch
         return detections
-
 
     def get_object_tracks(self, frames, read_from_stub = False, stub_path=None):
         
@@ -76,4 +80,87 @@ class Tracker:
 
         return(tracks)
     
+    def draw_ellipse(self, frame, bbox, color, track_id=None):
+        y2 = int(bbox[3])
+        x_center, _ = get_center_of_bbox(bbox)
+        width = get_bbox_width(bbox)
+
+        cv2.ellipse(
+            frame,
+            center=(x_center,y2),
+            axes=(int(width), int(0.35*width)),
+            angle=0.0,
+            startAngle=-45,
+            endAngle=235,
+            color = color,
+            thickness=2,
+            lineType=cv2.LINE_4
+        )
+
+        rectangle_width = 40
+        rectangle_height = 20
+        x1_rect = x_center - (rectangle_width//2)
+        x2_rect = x_center + (rectangle_width//2)
+        y1_rect = (y2 - rectangle_height//2)
+        y2_rect = (y2 + rectangle_height//2)
+        
+        if track_id is not None:
+            cv2.rectangle(frame,
+                          (int(x1_rect), int(y1_rect)),
+                          (int(x2_rect), int(y2_rect)),
+                          color,
+                          cv2.FILLED
+                          )
+            x1_text = x1_rect + 12
+            if track_id > 99:
+                x1_text -= 10
+            cv2.putText(frame, 
+                        f"{track_id}",
+                        (int(x1_text),int(y1_rect+15)),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6,
+                        (0,0,0),
+                        2
+                        )
+        return frame
+
+    def draw_triangle(self, frame, bbox, color):
+        y = int(bbox[1])
+        x,_ = get_center_of_bbox(bbox)
+        triangle_points = np.array([
+            [x,y],
+            [x-10,y-20],
+            [x+10, y-20]
+        ])
+
+        cv2.drawContours(frame, [triangle_points], 0, color, cv2.FILLED)
+        cv2.drawContours(frame, [triangle_points], 0, (0,0,0),2)
+
+        return frame
+
+    def draw_annotations(self,video_frames, tracks):
+        output_video_frames= []
+        for frame_num, frame in enumerate(video_frames):
+            frame = frame.copy()
+
+            player_dict = tracks["players"][frame_num]
+            ball_dict = tracks["ball"][frame_num]
+            referee_dict = tracks["referees"][frame_num]
+
+            # Draw Players
+            for track_id, player in player_dict.items():
+                color = player.get("team_color",(0,0,255))
+                frame = self.draw_ellipse(frame, player["bbox"],color, track_id)
+
+            # Draw Referees
+            for _, referee in referee_dict.items():
+                frame = self.draw_ellipse(frame, referee['bbox'],(0,255,255))
+
+            #Draw Ball
+            for track_id, ball in ball_dict.items():
+                frane = self.draw_triangle(frame, ball['bbox'], (0,255,0))
+
+            output_video_frames.append(frame)
+
+        return output_video_frames
 
